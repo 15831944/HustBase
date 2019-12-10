347fa4a1671ff0800bd1b4ca2b87bd86c2369796
wontun12 <wontun12@gmail.com>
2019-12-10 09:57:51 +0800

    W: change function name

--- IX_Manager.cpp
+++ IX_Manager.cpp
@@ -1,6 +1,24 @@
 #include "stdafx.h"
 #include "IX_Manager.h"
 
+RC CreateIndex(const char* fileName, AttrType attrType, int attrLength){
+	auto pf_fileHandle = new PF_FileHandle();
+	RC createFileCode = CreateFile(fileName);
+	if (createFileCode != SUCCESS) {
+		delete pf_fileHandle;
+		return createFileCode;
+	}
+	OpenFile((char*)fileName, pf_fileHandle);
+}
+
+RC OpenIndex(const char* fileName, IX_IndexHandle* indexHandle){
+
+}
+
+RC CloseIndex(IX_IndexHandle* indexHandle) {
+
+}
+
 RC OpenIndexScan(IX_IndexScan *indexScan,IX_IndexHandle *indexHandle,CompOp compOp,char *value){
 	return SUCCESS;
 }
